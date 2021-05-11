d8c31515e21c2efc9dda29605b0690f47285aa7a
Emmacudz <emma.cuddy@gmail.com>
2021-05-11 21:26:13 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -81,3 +81,14 @@ get '/finstagram_posts/:id' do
     erb(:"finstagram_posts/show")
 end
 
+post '/comments' do
+    
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id})
+
+    comment.save
+    
+    redirect(back)
+end
