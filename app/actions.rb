b54d656411414c650391f768dd9db3ec1e43236d
Emmacudz <emma.cuddy@gmail.com>
2021-05-11 00:02:12 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -56,8 +56,7 @@ end
 
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
-    @current_user = User.find_by(id: session[:user_id])
-    erb(:index)
+        erb(:index)
 end
 
 get '/finstagram_posts/new' do
